4a9dde4652b3d6a661a26765e15b10642ce1307b
santraez <santraez@gmail.com>
2022-10-12 15:04:43 -0500

    modified uri

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_PORT } = process.env;
 //const MONGODB_URI = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}/${MONGODB_DATABASE}?authSource=admin`;
-const MONGODB_URI = `mongodb://${{ MONGODB_USER }}:${{ MONGODB_PASSWORD }}@${{ MONGODB_HOST }}:${{ MONGODB_PORT }}`;
+const MONGODB_URI = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MONGODB_PORT}`;
 
 const connect = async() => {
   try {
